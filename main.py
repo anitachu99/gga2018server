03b10d7d100df5985c0bf0e098b95870a02da5db
Porrith Suong <psuong95@gmail.com>
2018-01-27 12:46:18 -0500

    Updates gitignore.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+    """
+    Returns the index of the function.
+    """
+    return "TODO"
+
 
 if __name__ == '__main__':
   app.run()
